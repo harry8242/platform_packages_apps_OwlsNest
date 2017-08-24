64765b828f85590f69830fce0fa0ea9e279634a0
harry8242 <mohamedharis97@gmail.com>
2017-08-24 17:45:01 +0000

    OwlsNest : Support Color engine

--- src/com/aosip/owlsnest/OwlsNestSettings.java
+++ src/com/aosip/owlsnest/OwlsNestSettings.java
@@ -1,6 +1,7 @@
 package com.aosip.owlsnest;
 
 import android.app.Activity;
+import android.app.ThemeManager;
 import android.os.Bundle;
 import android.content.Context;
 import android.provider.Settings;
@@ -11,6 +12,7 @@ import android.preference.PreferenceScreen;
 import android.text.TextUtils;
 
 import com.android.settings.dashboard.SummaryLoader;
+import com.android.settings.display.ThemePreference;
 
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 
@@ -21,6 +23,9 @@ import com.android.settings.Utils;
 public class OwlsNestSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
     private static final String TAG = "OwlsNestSettings";
+    private static final String KEY_THEME = "theme";
+
+    private ThemePreference mThemePreference;
 
     @Override
     protected int getMetricsCategory() {
@@ -32,6 +37,22 @@ public class OwlsNestSettings extends SettingsPreferenceFragment implements
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.owlsnest);
+
+        mThemePreference = (ThemePreference) findPreference(KEY_THEME);
+        if (mThemePreference != null) {
+            final int accentColorValue = Settings.Secure.getInt(getContext().getContentResolver(),
+                    Settings.Secure.THEME_ACCENT_COLOR, 1);
+            final int primaryColorValue = Settings.Secure.getInt(getContext().getContentResolver(),
+                    Settings.Secure.THEME_PRIMARY_COLOR, 2);
+            mThemePreference.setSummary(PreviewSeekBarPreferenceFragment.getInfoText(getContext(),
+                    false, accentColorValue, primaryColorValue) + ", " +
+                    PreviewSeekBarPreferenceFragment.getInfoText(getContext(), true,
+                    accentColorValue, primaryColorValue));
+            if (ThemeManager.isOverlayEnabled()) {
+                mThemePreference.setEnabled(false);
+                mThemePreference.setSummary(R.string.oms_enabled);
+            }
+        }
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
